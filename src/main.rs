224d3ff6a945d76446e231fb45f6b3a437a9320b
Heejong Ahn <heejongahn@gmail.com>
2019-09-08 21:41:38 +0900

    Complete “Accept Multiple Commands”

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use std::io::stdin;
+use std::io::{stdin, stdout, Write};
 use std::process::Command;
 
 fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
+    loop {
+        // use the `>` character as the prompt
+        // need to explicitly flush this to ensure it prints before read_line
+        print!("> ");
+        stdout().flush();
 
-    // read_line leaves a trailing newline, which trim removes
-    let command = input.trim();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
 
-    Command::new(command).spawn().unwrap();
+        let command = input.trim();
+
+        let mut child = Command::new(command).spawn().unwrap();
+
+        // don't accept another command until this one completes
+        child.wait();
+    }
 }
